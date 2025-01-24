95bcf1527fa66b87ffea65044bb4134c382dd764
MichaelT-W23 <michael.totaro@icloud.com>
2025-01-24 00:16:15 -0500

    Genre queries

--- queries/BookQuery.py
+++ queries/BookQuery.py
@@ -40,3 +40,33 @@ class BookQuery:
                 genre=book.genre, 
                 author_id=book.author_id
             )
+    
+    @strawberry.field
+    def get_all_genres(self) -> list[str]:
+        with Session(engine) as session:
+            statement = select(Book.genre).distinct()
+            all_genres = session.exec(statement).all()
+            
+            return all_genres
+        
+    @strawberry.field
+    def get_books_by_genre(self, genre: str) -> list[BookType]:
+        with Session(engine) as session:
+            statement = select(Book).where(Book.genre == genre)
+            results = session.exec(statement).all()
+
+            if not results:
+                raise HTTPException(status_code=404, detail="No books found for the specified genre")
+            
+            return [
+                BookType(
+                    id=book.id,
+                    title=book.title,
+                    publicationYear=book.publicationYear,
+                    genre=book.genre,
+                    author_id=book.author_id
+                )
+                for book in results
+            ]
+    
+    
